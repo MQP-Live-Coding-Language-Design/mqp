3c59a66afff080f3716378caf2808eb748d16e49
mariana0pachon <mpachonpuentes@wpi.edu>
2019-09-16 01:29:26 -0400

    Set up react app with parcel bundler

---
+++ app/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Homepage from '~components/Homepage';
+import Tutorial from '~components/Tutorial';
+
+const App = () => (
+  <div>
+    <Homepage />
+    <Tutorial />
+  </div>
+);
+
+export default App;
