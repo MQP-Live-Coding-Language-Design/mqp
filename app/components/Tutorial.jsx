0b334bd80d170784c2bdcfc58be8892483bb5cb5
Henry Frishman <henry@Henrys-MacBook-Air.local>
2019-09-25 14:58:17 -0400

    Updated and spread out the tutorial. Added some content and separated some of the longer steps into multiple shorter ones

--- app/components/Tutorial.jsx
+++ app/components/Tutorial.jsx
@@ -16,25 +16,34 @@ const Tutorial = () => (
       css={{ maxWidth: 500, height: 200 }}
     >
 
-      To start playing, simply create a sequence of notes and send it to an instrument.
-      A note is any letter between 'a' and 'g'. This sequence can then be played on an instrument
-      by writing '>>' followed by the instrument name as seen below:
+      To start playing, simply create a sequence of notes surrounded by a " on either side and send it to an instrument.
+      A note can be any letter between 'a' and 'g'. This sequence can then be played on an instrument
+      by writing '>>' followed by the instrument name. As seen below, we create a sequence of notes a b c, and play it on a triangle:
       <br />
       <TutorialEditor
         width="500"
         height="100"
-        text='"a b c" >> soft'
+        text='"a b c" >> triangle'
       />
       <br />
       <br />
       <br />
-        Both the notes and the entire sequence of notes can be edited. You make a note sharp or flat
-      by adding a '#' or 'b' respectively to the end of the note. The octave of a note defaults to
+        Both the notes and the entire sequence of notes can be edited. You can make a note sharp or flat
+      by adding a '#' or 'b' (e.g. 'a#', 'cb') respectively in front of the note. The octave of a note defaults to
       4, but can be changed either by specifying an octave (e.g. 'a5' sets the octave to 5) or by
       using +/- to raise and lower this value (e.g. 'a+' sets the octave to 5).
       <br />
+      <TutorialEditor
+        width="500"
+        height="100"
+        text='"a+ b3 c#" >> triangle'
+      />
+      <br />
+      <br />
+      <br />
+
         Entire sequences can be modified using the '>>' followed by the modification. The modifiers
-      are "octave," "pitch," and "duration". Octave takes either an integer or a series of +/- symbols.
+      are currently "octave," "pitch," and "duration". Octave takes either an integer or a series of +/- symbols.
       The +/- move the value from the default of 4 (e.g. ">> octave 6" and ">> octave ++"
       both set the octave to 6). Pitch takes either a series of +/- or a signed integer for the amount to move
       the pitch up or down (e.g. ">> pitch --" and ">> pitch -2" both move the pitch down by 2).
@@ -44,7 +53,7 @@ const Tutorial = () => (
       <TutorialEditor
         width="500"
         height="100"
-        text='"a b c" >> octave 2 >> pitch -- >> duration .25 >> soft'
+        text='"a b c" >> octave 2 >> pitch -- >> duration .25 >> triangle'
       />
       <br />
       <br />
@@ -53,12 +62,49 @@ const Tutorial = () => (
         Like a sequence of notes, the instruments (called parts) can be modified.
       This is done by following the instrument with "^" and then the part modifier and value.
       The part modifiers are as follows: distort, lo, hi, and bandpass. There is an example below:
-
+      <br />
       <TutorialEditor
         width="500"
         height="100"
-        text='"a b c" >> octave 2 >> pitch -- >> duration .25 >> soft'
+        text='"a b c" >> octave 2 >> pitch -- >> duration .25 >> triangle'
       />
+      <br />
+      <br />
+      <br />
+
+        You can play the same notes with multiple instruments as well. See below we play 'a b c' on both a triangle and a soft synth
+      <br />
+      <TutorialEditor
+        width="500"
+        height="100"
+        text='"a b c" >> triangle >> soft'
+      />
+      <br />
+      <br />
+      <br />
+
+        Make multiple patterns!
+      <br />
+      <TutorialEditor
+        width="500"
+        height="100"
+        text='"a b c" >> triangle
+         "c e g" >> soft'
+      />
+      <br />
+      <br />
+      <br />
+
+        Feel free to combine anything you've used!
+      <br />
+      <TutorialEditor
+        width="500"
+        height="100"
+        text='"a# d f5" >> octave++ >> triangle'
+      />
+      <br />
+      <br />
+      <br />
     </Box>
   </div>
 );
