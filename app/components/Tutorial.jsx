c558c238d57d20cea86438e79f048149653a1950
Henry Frishman <henry@Henrys-MacBook-Air.local>
2019-09-25 15:45:03 -0400

    Updated tutorial to be functional, added it to the main page.

--- app/components/Tutorial.jsx
+++ app/components/Tutorial.jsx
@@ -4,7 +4,8 @@ import blueGrey from '@material-ui/core/colors/blueGrey';
 import { ThemeProvider } from '@material-ui/styles';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
-import TutorialEditor from './TutorialEditor';
+// import TutorialEditor from './TutorialEditor';
+import PlayBox from './PlayBox';
 
 const theme = createMuiTheme({
   palette: {
@@ -51,11 +52,7 @@ const Tutorial = () => (
       A note is any letter between 'a' and 'g'. This sequence can then be played on an instrument
       by writing '>>' followed by the instrument name as seen below:
         <br />
-        <TutorialEditor
-          width="500"
-          height="100"
-          text='"a b c" >> soft'
-        />
+        <PlayBox id="firtsTutBox" value='"c e g" >> soft' />
         <br />
         <br />
         <br />
@@ -64,71 +61,58 @@ const Tutorial = () => (
       4, but can be changed either by specifying an octave (e.g. 'a5' sets the octave to 5) or by
       using +/- to raise and lower this value (e.g. 'a+' sets the octave to 5).
         <br />
+        <PlayBox id="secondTutBox" value='"c# eb g+" >> triangle' />
+        <br />
+        <br />
+        <br />
+
         Entire sequences can be modified using the '>>' followed by the modification. The modifiers
       are currently "octave," "pitch," and "duration". Octave takes either an integer or a series of +/- symbols.
-      The +/- move the value from the default of 4 (e.g. ">> octave 6" and ">> octave ++"
+      The +/- move the value from the default of 4 (e.g. ">> octave +6" and ">> octave ++"
       both set the octave to 6). Pitch takes either a series of +/- or a signed integer for the amount to move
       the pitch up or down (e.g. ">> pitch --" and ">> pitch -2" both move the pitch down by 2).
       Duration sets the time for each note in the sequence. It takes either a fraction or a decimal
       (e.g. ">> duration .5" and ">> duration 1/2" do the same thing). Examples are seen below:
-      <br />
-      <TutorialEditor
-        width="500"
-        height="100"
-        text='"a b c" >> octave 2 >> pitch -- >> duration .25 >> triangle'
-      />
-      <br />
-      <br />
-      <br />
+        <br />
+        <PlayBox id="modTutBox" value='"c e g" >> octave +2 >> pitch -- >> duration .25 >> triangle' />
+        <br />
+        <br />
+        <br />
 
-        Like a sequence of notes, the instruments (called parts) can be modified.
+
+        {/*
+      Like a sequence of notes, the instruments (called parts) can be modified.
       This is done by following the instrument with "^" and then the part modifier and value.
       The part modifiers are as follows: distort, lo, hi, and bandpass. There is an example below:
-      <br />
-      <TutorialEditor
-        width="500"
-        height="100"
-        text='"a b c" >> octave 2 >> pitch -- >> duration .25 >> triangle'
-      />
-      <br />
-      <br />
-      <br />
-
-        You can play the same notes with multiple instruments as well. See below we play 'a b c' on both a triangle and a soft synth
-      <br />
-      <TutorialEditor
-        width="500"
-        height="100"
-        text='"a b c" >> triangle >> soft'
-      />
-      <br />
-      <br />
-      <br />
+        <br />
+        <PlayBox id="thirdTutBox" value='"c e g" >> octave 2 >> pitch -- >> duration .25 >> triangle' />
+        <br />
+        <br />
+        <br />
+*/}
+        You can play the same notes with multiple instruments as well. See below we play 'c e g' on both a triangle and a soft synth
+        <br />
+        <PlayBox id="fourthTutBox" value='"c e g" >> triangle >> soft' />
+        <br />
+        <br />
+        <br />
 
         Make multiple patterns!
-      <br />
-      <TutorialEditor
-        width="500"
-        height="100"
-        text='"a b c" >> triangle
-         "c e g" >> soft'
-      />
-      <br />
-      <br />
-      <br />
+        <br />
+        <PlayBox id="fifthTutBox" value='"c e g" >> triangle >> saw "g b d+" >> soft' />
+        <br />
+        <br />
+        <br />
 
         Feel free to combine anything you've used!
-      <br />
-      <TutorialEditor
-        width="500"
-        height="100"
-        text='"a# d f5" >> octave++ >> triangle'
-      />
-      <br />
-      <br />
-      <br />
-    </Box>
-  </div>
+        <br />
+        <PlayBox id="sixthTutBox" value='"c# e g5" >> octave++ >> triangle' />
+        <br />
+        <br />
+        <br />
+      </Box>
+    </Typography>
+  </ThemeProvider>
 );
 
 export default Tutorial;
