e3adbdd5099738cc689d8266c3e702f27f3ae80e
bjanderson1 <bjanderson@wpi.edu>
2019-09-30 12:36:57 -0400

    fixed

--- app/components/tutorial/Tutorial.jsx
+++ app/components/tutorial/Tutorial.jsx
@@ -138,8 +138,8 @@ const Tutorial = () => (
           value={`"ab f db eb" >> duration 2 >> octave - >> soft >> pitch +7 >> soft
 "o _ x _" >> duration .5 >> play
 "c+ ab g ab c+ ab g ab db ab g ab bb ab g ab" >> duration .5 >> triangle
-"ab _ _ eb _ _ c _ _ eb _ _ ab _ _ f _ _ db _ f _ ab _ bb _ _ ab _ _ g _" >> octave >> duration 1/4 >> triangle
-"ab ab eb+" >> duration 1/4 >> saw
+"ab _ _ eb _ _ c _ _ eb _ _ ab _ _ f _ _ db _ f _ ab _ bb _ _ ab _ _ g _" >> octave >> duration 1/4 >> saw
+"ab ab eb+" >> duration 1/4 >> triangle
 "ab _ _ eb _ _ f ab ~ _ _ g _ _ ab bb" >> octave ++ >> duration .5 >> triangle & > pingpong .25
 "- - - - -- - - -" >> duration .5 >> play`}
         />
