d89a73b3c36c613b91bf95d9e465a589ac4689a7
Henry Frishman <henry@Henrys-MacBook-Air.local>
2019-10-02 01:15:07 -0400

    Playboxes have their own tone

--- app/components/common/PlayBox.jsx
+++ app/components/common/PlayBox.jsx
@@ -3,21 +3,63 @@ import PropTypes from 'prop-types';
 import Editor from '@monaco-editor/react';
 import Button from '@material-ui/core/Button';
 
+const Tone = require('tone');
+const peg = require('../../../language/language.js');
+
 const PlayBox = ({ id, value }) => {
   const [isEditorReady, setIsEditorReady] = useState(false);
   const [parseValue, setParseValue] = useState(value);
-  const [startState, setStartValue] = useState('Start');
+  const [buttonState, setButtonState] = useState('Start');
   const valueGetter = useRef();
+  const runningParts = {};
+  const loaded = true;
+
 
   function handleEditorDidMount(_valueGetter) {
     setIsEditorReady(true);
     valueGetter.current = _valueGetter;
   }
 
-  function changeValueToParse() {
+  function stop() {
+    if (id in runningParts) {
+      runningParts[id].forEach((part) => {
+        part.stop();
+      });
+    }
+    delete runningParts[id];
+  }
+
+  function start() {
+    stop(id);
+    const parts = peg.parse(parseValue);
+    runningParts[id] = parts;
+
+    if (loaded && Tone.context.state === 'running') {
+      parts.forEach((part) => { part.start(); });
+    } else {
+      console.log('unloaded');
+    }
+  }
+
+
+  function buttonClick() {
     setParseValue(valueGetter.current());
-    if (startState === 'Start') setStartValue('Stop');
-    else setStartValue('Start');
+
+    if (Tone.context.state !== 'running') {
+      Tone.context.resume();
+    }
+    if (Tone.Transport.state !== 'started') {
+      Tone.Transport.start();
+      Tone.Transport.seconds = Tone.context.now();
+    }
+
+    if (buttonState === 'Start') {
+      start();
+      setButtonState('Stop');
+    } else {
+      stop();
+      setButtonState('Start');
+    }
   }
 
   return (
@@ -29,8 +71,8 @@ const PlayBox = ({ id, value }) => {
         theme="dark"
         editorDidMount={handleEditorDidMount}
       />
-      <Button className={startState} type="button" value={parseValue} onClick={changeValueToParse} disabled={!isEditorReady} id="trigger">
-        {startState}
+      <Button className={buttonState} type="button" value={parseValue} onClick={buttonClick} disabled={!isEditorReady} id="trigger">
+        {buttonState}
       </Button>
     </div>
   );
