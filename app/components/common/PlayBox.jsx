ea1cba81a129d93b528b2cb5952a3f36cec68bff
norman-delorey <ndelorey11@gmail.com>
2019-10-03 20:51:29 -0400

    Finally got color coding to work

--- app/components/common/PlayBox.jsx
+++ app/components/common/PlayBox.jsx
@@ -1,11 +1,38 @@
 import React, { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
-import Editor from '@monaco-editor/react';
+import Editor, { monaco } from '@monaco-editor/react';
 
 const PlayBox = ({ id, value }) => {
   const [isEditorReady, setIsEditorReady] = useState(false);
   const [parseValue, setParseValue] = useState(value);
   const valueGetter = useRef();
+  monaco.init()
+    .then((monaco) => {
+      monaco.languages.register({ id: 'sicko-mode' });
+
+      monaco.languages.setMonarchTokensProvider('sicko-mode', {
+        tokenizer: {
+          root: [
+            [/\ffff/, 'custom-error'],
+            [/>> (octave|pitch|duration) [+-./0-9]*[0-9]*/, 'modifier'],
+            [/>> (soft|triangle|saw|play)/, 'instrument'],
+            [/"[a-zA-Z 0-9+#-_~()]+"[+-]*[0-9]*/, 'notes'],
+          ],
+        },
+      });
+
+      monaco.editor.defineTheme('sicko-theme', {
+        base: 'vs-dark',
+        inherit: false,
+        rules: [
+          { token: 'instrument', foreground: 'D7BDE2' },
+          { token: 'custom-error', foreground: 'ff0000', fontStyle: 'bold' },
+          { token: 'modifier', foreground: 'D6EAF8' },
+          { token: 'notes', foreground: 'A3E4D7' },
+        ],
+      });
+    })
+    .catch((error) => console.error('An error occurred during initialization of Monaco: ', error));
 
   function handleEditorDidMount(_valueGetter) {
     setIsEditorReady(true);
@@ -19,10 +46,11 @@ const PlayBox = ({ id, value }) => {
   return (
     <div className="playBox" id={id}>
       <Editor
+        language="sicko-mode"
+        theme="sicko-theme"
         height="10vh"
-        width="50vw"
         value={value}
-        theme="dark"
+        width="50vw"
         editorDidMount={handleEditorDidMount}
       />
       <button type="button" value={parseValue} onClick={changeValueToParse} disabled={!isEditorReady} id="Start">Start</button>
