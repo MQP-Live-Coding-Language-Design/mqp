2607a2a12ca0ab1cd53b2220f06d0862975a4c5b
bjanderson1 <bjanderson@wpi.edu>
2019-10-17 23:29:46 -0400

    Updates

--- app.78399e21.js
+++ app.78399e21.js
@@ -65040,11 +65040,12 @@ var PlayBox = function PlayBox(_ref) {
   function handleEditorDidMount(_valueGetter, editor) {
     setModel(editor._modelData.model);
     valueGetter.current = _valueGetter;
-    var time;
-    editor.onDidChangeModelContent(function () {
+    /*
+    let time;
+    editor.onDidChangeModelContent(() => {
       clearTimeout(time);
       box.editor.setModelMarkers(editor._modelData.model, 'test', []);
-      time = setTimeout(function () {
+      time = setTimeout(() => {
         try {
           peg.parse(valueGetter.current());
         } catch (error) {
@@ -65054,11 +65055,13 @@ var PlayBox = function PlayBox(_ref) {
             endLineNumber: error.location.end.line,
             endColumn: error.location.end.column,
             message: error.message,
-            severity: box.MarkerSeverity.Error
+            severity: box.MarkerSeverity.Error,
           }]);
         }
       }, 1500);
     });
+    */
+
     setIsEditorReady(true);
   }
 
@@ -65145,7 +65148,7 @@ exports.default = _default;
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.welcome = exports.examplesong = exports.tutorialconclusion = exports.examplepatterns = exports.tutorialpatterns = exports.tutorialmult = exports.exampledrums = exports.tutorialdrums = exports.tutorialduration = exports.tutorialmods = exports.tutorialaltchars = exports.tutorial2 = exports.tutorial1 = void 0;
+exports.examplesong2 = exports.welcome = exports.examplesong = exports.tutorialconclusion = exports.examplepatterns = exports.tutorialpatterns = exports.tutorialmult = exports.exampledrums = exports.tutorialdrums = exports.tutorialduration = exports.tutorialmods = exports.tutorialaltchars = exports.tutorial2 = exports.tutorial1 = void 0;
 var tutorial1 = 'To start playing, simply create a sequence of notes and send it to an instrument. A note is any letter between `a` and `g`. This sequence can then be played on an instrument by writing `>>` followed by the instrument name as seen below. A full list of instruments can be found in the Documentation tab.';
 exports.tutorial1 = tutorial1;
 var tutorial2 = 'Notes can be modified before you play them. You make a note sharp or flat adding a `#` or `b` respectively to the end of the note. The octave of a note defaults to 4, but can be changed either by specifying an octave (e.g. `a5` sets the octave to 5) or by using `+`/`-` to raise and lower this value (e.g. `a+` sets the octave to 5).';
@@ -65172,6 +65175,8 @@ var examplesong = '"ab f db eb" >> duration 2 >> octave - >> soft >> pitch +7 >>
 exports.examplesong = examplesong;
 var welcome = 'Language Name\n===\nWelcome to Language! This is a live coding language designed for coders and non-coders alike. Below is an editor in which you can see the code for some music. It might look complicated but don\'t worry, it will all make sense soon. Feel free to alter the text in this or any other box. Press stop then start again to hear your changes.';
 exports.welcome = welcome;
+var examplesong2 = '"f# d a e" >> duration 4 >> octave -- >> save bass2 >> pitch +7 >> save bass3\n"chord(!bass2 !bass3)" >> soft > volume 10\n\n"_ _ a g# a _ c#+ _" >> save mel1\n"_ _ a g# a _ d+ _" >> save mel2\n"_ _ g# f# g# _ b _" >> save mel3\n"_ _ f# e f# _ a _" >> save harm1\n"_ _ e e e _ a _" >> save harm2\n"_ _ e e e _ g# _" >> save harm3\n"(!mel1)*2 (!mel2)*2 (!mel1)*2 (!mel3)*2" >> duration .25 >> triangle\n"(!harm1)*4 (!harm2)*2 (!harm3)*2" >> duration .25 >> triangle\n\n"c#+ 5~ b a 8~ c#+ 5~ b b 8~" >> duration .5 >> fatsaw\n"a 5~ g# f# 8~ a 5~ g# g# 8~" >> duration .5 >> fatsaw\n"f# 5~ e d 8~ e 5~ e e 8~" >> duration .5 >> fatsaw\n\n"f# f# a f# f# a f# f#" >> save blip1\n"e e a e e a e e" >> save blip2\n"e e g# e e g# e e" >> save blip3\n"a a c#+ a a c#+ a a" >> save bliph1\n"a a d+ a a d+ a a" >> save bliph2\n"a a b a a b a a" >> save bliph3\n"g# g# b g# g# b g# g#" >> save bliph4\n"(chord(!blip1 !bliph1))*2 (chord(!blip1 !bliph2))*2 (chord(!blip2 !bliph1))*2 chord((!blip3)*2 (!bliph3 !bliph4))"\n>> duration .25 >> octave ++ >> saw > volume -10\n"chord(!blip1 !bliph1) chord(!blip1 !bliph2) chord(!blip2 !bliph1) chord(!blip3 !bliph4)"\n>> octave + >> duration .5 >> triangle wave sine > volume -5\n\n"h h sn h" >> duration .5 >> drums\n"cr" >> duration 16 >> drums > volume 5\n"k" >> electricdrums';
+exports.examplesong2 = examplesong2;
 },{}],"components/tutorial/Tutorial.jsx":[function(require,module,exports) {
 "use strict";
 
@@ -65282,7 +65287,7 @@ var Tutorial = function Tutorial() {
     source: _TutorialText.tutorialconclusion
   }), _react.default.createElement(_PlayBox.default, {
     id: "sixthTutBox",
-    value: _TutorialText.examplesong
+    value: _TutorialText.examplesong2
   }))));
 };
 
@@ -65607,7 +65612,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62412" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64244" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
