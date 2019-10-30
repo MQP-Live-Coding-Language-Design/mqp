e9e0d83cc3b21bb820a754d257be56a0c1c12f7f
Mariana Pachon <marianapachon@autoreg-213268.dyn.wpi.edu>
2019-10-30 10:31:03 -0400

    redirecting

--- app/App.jsx
+++ app/App.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Homepage from './components/common/Homepage';
 import SignInPage from './components/googleoauth/SignInPage';
+import UTHomepage from './components/usertesting/UTHomepage';
+import Playground from './components/playground/Playground';
 
 const App = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={SignInPage} />
       <Route path="/success" component={Homepage} />
+      <Route path="/usertesting" component={UTHomepage} />
+      <Route path="/playground" component={Playground} />
     </Switch>
   </BrowserRouter>
 );
--- app/components/common/Homepage.jsx
+++ app/components/common/Homepage.jsx
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cookies from 'universal-cookie';
 import { Redirect } from 'react-router-dom';
-import UTHomepage from '../usertesting/UTHomepage';
+import Button from '@material-ui/core/Button';
+
 
 const Homepage = () => {
+  const [reroute, setReroute] = useState('homepage');
+
+  const userTestReroute = () => {
+    setReroute('usertest');
+  };
+
+  const playgroundReroute = () => {
+    setReroute('playground');
+  };
+
+  const renderSwitch = () => {
+    switch (reroute) {
+      case 'usertest':
+        return <Redirect to="/usertest" />;
+      case 'playground':
+        return <Redirect to="/playground" />;
+      default:
+        return (
+          <>
+            <Button type="button" onClick={userTestReroute}>
+  user test
+            </Button>
+            <Button type="button" onClick={playgroundReroute}>
+  playground
+            </Button>
+          </>
+        );
+    }
+  };
+
   const cookies = new Cookies();
   if (cookies.get('email')) {
-    return <UTHomepage />;
+    return renderSwitch();
   }
   return <Redirect to="/" />;
 };
