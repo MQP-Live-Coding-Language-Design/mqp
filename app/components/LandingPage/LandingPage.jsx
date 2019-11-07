6b6ba498e322486590aba5653f9e419db5eaf1dc
mariana0pachon <mariana0pachon@gmail.com>
2019-11-07 15:25:36 -0500

    yay

--- app/components/LandingPage/LandingPage.jsx
+++ app/components/LandingPage/LandingPage.jsx
@@ -9,60 +9,23 @@ const Wrapper = styled.section`
   display:flex;
   justify-content:space-around;
   align-content: center;
-  height: 98vh;
+  height: 90vh;
 `;
 
 const Half = styled.section`
   display: flex;
-  width: 40vw;
+  width: 30vw;
   flex-wrap: wrap;
   align-content: center;
-  justify-content: space-around;
+  justify-content: center;
 `;
 
-const LandingPage = () => {
-  const [route, setRoute] = useState('landingpage');
-
-  const reroute = (newRoute) => {
-    setRoute(newRoute);
-  };
-
-  const renderSwitch = () => {
-    switch (route) {
-      case 'playground':
-        return <Redirect to="/playground" />;
-      case 'tutorial':
-        return <Redirect to="/tutorial" />;
-      case 'documentation':
-        return <Redirect to="/documentation" />;
-      case 'exercises':
-        return <Redirect to="/exercises" />;
-      default:
-        return (
-          <Wrapper>
-            <Half>
-              <ReactMarkdown source={intro} />
-            </Half>
-            <Half>
-              <Button variant="outlined" type="button" onClick={() => reroute('playground')}>
-              Playground
-              </Button>
-              <Button variant="outlined" type="button" onClick={() => reroute('tutorial')}>
-              Tutorial
-              </Button>
-              <Button variant="outlined" type="button" onClick={() => reroute('documentation')}>
-              Documentation
-              </Button>
-              <Button variant="outlined" type="button" onClick={() => reroute('exercises')}>
-              Exercises
-              </Button>
-            </Half>
-          </Wrapper>
-        );
-    }
-  };
-
-  return renderSwitch();
-};
+const LandingPage = () => (
+  <Wrapper>
+    <Half>
+      <ReactMarkdown source={intro} />
+    </Half>
+  </Wrapper>
+);
 
 export default LandingPage;
